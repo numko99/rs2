c8702b3943723c05b4c99c85a3f7c3127330eef3
Admir Numanović <admir.numanovic@breakpoint.software>
2024-08-25 23:17:14 +0200

    Minor change

--- Iter.Api/ui/Iter.Desktop/lib/widgets/logo.dart
+++ Iter.Api/ui/Iter.Desktop/lib/widgets/logo.dart
@@ -11,7 +11,7 @@ class Logo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Text(
-      'Da lila',
+      'ITer',
       style: TextStyle(
         fontSize: fontSize,
         fontWeight: FontWeight.bold,
